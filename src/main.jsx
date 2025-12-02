c8f527b3aa6b66c25076b6a743e7076efed71153
vernittyagi <vernittyagi@Vernits-MacBook-Air.local>
2025-12-02 11:18:06 +0530

    fixing typo in contact.jsx name on main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Contact from './pages/contact.jsx'
+import Contact from './pages/Contact.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
